500fd5c5d13edfb7d22d809b8edcb4474943ec2c
Chevalier-Maxime <chevalier.maxime@outlook.com>
2017-09-30 14:33:49 -0400

    test Maven

---
+++ src/main/java/Part1/Parser.java
@@ -0,0 +1,21 @@
+package Part1;
+
+import java.net.URL;
+
+public class Parser {
+    private URL url;
+    private int currentIndex;
+    private final static int START_INDEX = 1;
+    private final static int FINAL_INDEX = 1975;
+
+    public Parser(URL url) {
+        this.url = url;
+    }
+
+
+    public RawEntry getNextSpell(){
+        return null;
+    }
+
+
+}
