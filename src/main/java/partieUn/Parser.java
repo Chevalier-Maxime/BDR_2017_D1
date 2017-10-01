91725aaa9537e5799dc3ba759aaee594fb1428bd
Chevalier-Maxime <chevalier.maxime@outlook.com>
2017-10-01 00:02:52 -0400

    Fonction de parsing faite, faire la fonction next()

--- src/main/java/partieUn/Parser.java
+++ src/main/java/partieUn/Parser.java
@@ -1,5 +1,6 @@
 package partieUn;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -30,9 +31,70 @@ public class Parser {
 
         //Components
         spellName = doc.select("P.SPDet").get(2);
-        System.out.printf(spellName.text());
+        System.out.println(spellName.text());
 
+        //Spell resistance
+        spellName = doc.select("P.SPDet").get(6);
+        System.out.println(spellName.text());
         //current_index++;
+
+        getSpellResistance(doc);
+        System.out.println(getSpellResistance(doc));
         return null;
+    }
+
+    private String getSpellName(Document doc){
+        Element spellName = doc.select("div.heading").first();
+        return spellName.text();
+    }
+
+    private String[] getClasses(Document doc){
+        Element spellName = doc.select("P.SPDet").first();
+        String string = spellName.text();
+        string = string.substring(string.lastIndexOf(';') + 7);
+        String stringFormatted = string.replace('/',',');
+        stringFormatted = stringFormatted.replaceAll("\\d|\\s","");
+
+        return stringFormatted.split(",");
+
+    }
+
+    private String[] getLevels(Document doc){
+        Element spellName = doc.select("P.SPDet").first();
+        String string = spellName.text().replace('/',',');
+        String stringFormatted = string.substring(string.lastIndexOf(';') + 7);
+        stringFormatted = stringFormatted.replaceAll("[^0-9,]","");
+
+        String finalString = "";
+
+        char precedent = stringFormatted.charAt(0);
+        for(int i = 1 ; i < stringFormatted.length(); i++){
+            char current = stringFormatted.charAt(i);
+            if ((precedent == ',') && (i == 1)){
+                finalString+=stringFormatted.charAt(i) + "," + stringFormatted.charAt(i);
+            } else if ((precedent == ',') && current ==','){
+                finalString+=stringFormatted.charAt(i+1) + ",";
+            } else {
+                finalString+=stringFormatted.charAt(i);
+            }
+
+            precedent = current;
+        }
+        return stringFormatted.split(",");
+    }
+
+    private String[] getComponents(Document doc){
+        Element component = doc.select("P.SPDet").get(2);
+        String string = component.text();
+        string = string.substring(11).replaceAll("[^A-Z,]","");
+        return string.split(",");
+    }
+
+    private boolean getSpellResistance(Document doc){
+        Element spellResistance = doc.select("P.SPDet").get(6);
+        String string = spellResistance.text().substring(spellResistance.text().lastIndexOf(';'));
+        if (string.contains(" no")) return false;
+
+        return true;
     }
 }
