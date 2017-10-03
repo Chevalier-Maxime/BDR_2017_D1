ed6cc0c5cd54a21e78155b5b22da8554e8765b8d
Chevalier-Maxime <chevalier.maxime@outlook.com>
2017-10-03 11:11:25 -0400

    Modification en Map pour tout le raw entry : https://howtodoinjava.com/mongodb/java-mongodb-insert-documents-in-collection-examples/

--- src/main/java/partieUn/RawEntry.java
+++ src/main/java/partieUn/RawEntry.java
@@ -1,48 +1,29 @@
 package partieUn;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class RawEntry {
-    private Map<String,Integer> availableFor;
-    private String spellName;
-    //private String[] classes;
-    //private int[] level;
-    private String[] components;
-    private boolean spell_resistance;
+    private Map<String, Object> documentMap = new HashMap<>();
+    private Map<String,Integer> availableFor = new HashMap<>();
+
 
 
     public RawEntry(String spellName, String[] classes, int[] level, String[] components, boolean spell_resistance) {
-        this.spellName = spellName;
-        //this.classes = classes;
-        //this.level = level;
+
+        documentMap.put("SpellName",spellName);
+        documentMap.put("SpellResistance", spell_resistance);
+        documentMap.put("Components", components);
+
         for (int i = 0 ; i < classes.length ; i++) {
             availableFor.put(classes[i], level[i]);
         }
-        this.components = components;
-        this.spell_resistance = spell_resistance;
+
+        documentMap.put("AvailableFor", availableFor);
+
     }
 
-    public String getSpellName() {
-        return spellName;
-    }
-
-//    public String[] getClasses() {
-//        return classes;
-//    }
-//
-//    public int[] getLevel() {
-//        return level;
-//    }
-
-    public Map<String,Integer>  getAvaillability(){
-        return availableFor;
-    }
-
-    public String[] getComponents() {
-        return components;
-    }
-
-    public boolean isSpell_resistance() {
-        return spell_resistance;
+    public Map<String, Object> getDocumentMap() {
+        return documentMap;
     }
 }
