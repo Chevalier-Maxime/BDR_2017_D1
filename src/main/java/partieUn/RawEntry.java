9d8c619356d4ad529bf0a85d1ad5e4a03facff7e
Chevalier-Maxime <chevalier.maxime@outlook.com>
2017-10-05 16:22:40 -0400

    séparation RawEntry/MongoDB

--- src/main/java/partieUn/RawEntry.java
+++ src/main/java/partieUn/RawEntry.java
@@ -5,10 +5,25 @@ import org.bson.Document;
 import java.util.Arrays;
 
 public class RawEntry {
-    private Document doc = new Document();
+
+    private String spellName;
+    private String[] classes;
+    private int[] level;
+    private String[] components;
+    private boolean spell_resistance;
 
     public RawEntry(String spellName, String[] classes, int[] level, String[] components, boolean spell_resistance) {
 
+        this.spellName = spellName;
+        this.classes = classes;
+        this.level = level;
+        this.components = components;
+        this.spell_resistance = spell_resistance;
+
+    }
+
+    public Document getDoc() {
+        Document doc = new Document();
         doc.put("SpellName", spellName);
         doc.put("SpellResistance",spell_resistance);
         doc.put("Components", Arrays.asList(components));
@@ -18,10 +33,6 @@ public class RawEntry {
             availableFor.put(classes[i], level[i]);
         }
         doc.put("AvailableFor", availableFor);
-
-    }
-
-    public Document getDoc() {
         return doc;
     }
 
